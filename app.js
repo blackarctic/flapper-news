76c4221500eeb8d1a65749887d38fa9805fcecaf
Nick Galloway <blackarctic@icloud.com>
2015-09-23 20:36:21 -0500

    link submissions, styles, and services (factory) added

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+
 app.controller('MainCtrl', [
 '$scope',
-function ($scope) {
+'posts',
+function ($scope, posts) {
 	$scope.test = 'Hello world!';
 
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	];
+	$scope.posts = posts.posts;
 
 	$scope.addPost = function(){
+		//sanity check
 		if(!$scope.title || $scope.title === '') { return; }
-		$scope.posts.push({title: $scope.title, upvotes: 0});
+		//add them
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0});
+		//reset them
 		$scope.title = '';
+		$scope.link = '';
+	};
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
 	};
 }]);
