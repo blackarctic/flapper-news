4d4a9af5184702a6f566c6200ad756c4b22a31d4
Nick Galloway <blackarctic@icloud.com>
2015-10-04 15:22:37 -0500

    login and register pages created

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -27,6 +27,25 @@ app.config([
 					}]
 				}
 			});
+			.state('login', {
+				url: '/login',
+				templateUrl: '.login.html',
+				controller: 'AuthCtrl',
+				onEnter: ['$state', 'auth', function($state, auth) {
+					if(auth.isLoggedIn()) {
+						$state.go('home');
+					}
+				}]
+			}.state('register', {
+				url: '/register',
+				templateUrl: '.register.html',
+				controller: 'AuthCtrl',
+				onEnter: ['$state', 'auth', function($state, auth) {
+					if(auth.isLoggedIn()) {
+						$state.go('home');
+					}
+				}]
+			};
 
 		$urlRouterProvider.otherwise('home');
 
@@ -177,6 +196,32 @@ function($scope, posts, post) {
 	};
 
 }]);
+
+app.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'$auth',
+	function($scope, $state, auth) {
+		$scope.user = {};
+
+		$scope.register = function() {
+			auth.register($scope.user).error(function(error) {
+				$scope.error = error;
+			}).then(function() {
+				$state.go('home');
+			});
+		};
+
+		$scope.logIn = function() {
+			auth.logIn($scope.user).error(function(error) {
+				$scope.error = error;
+			}).then(function() {
+				$state.go('home');
+			});
+		};
+	};
+
+]);
 
 
 
